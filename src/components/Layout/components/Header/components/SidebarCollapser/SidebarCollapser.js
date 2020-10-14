a11f19b069f8ff8c00c6cf8f4caa45ed7adc3eb0
Mohamed Chiller <Chiller@MacBook-Pro-de-apple.local>
2020-10-14 15:31:13 +0100

    rtl  sidebar collapse trigger icon

--- src/components/Layout/components/Header/components/SidebarCollapser/SidebarCollapser.js
+++ src/components/Layout/components/Header/components/SidebarCollapser/SidebarCollapser.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import { bool, func } from 'prop-types';
+import { useLocaleState } from '../Localization';
 
 const propTypes = {
   handleCollapsed: func.isRequired,
   collapsed: bool.isRequired,
 };
 const SidebarCollapser = ({ handleCollapsed, collapsed }) => {
+  const { locale } = useLocaleState();
   return (
     <div className="sidebar-collapser">
-      {collapsed ? (
+      {(locale === 'ar' && !collapsed) || (locale !== 'ar' && collapsed) ? (
         <MenuUnfoldOutlined onClick={handleCollapsed} className="icons" />
       ) : (
         <MenuFoldOutlined onClick={handleCollapsed} className="icons" />
