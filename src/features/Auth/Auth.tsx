510548ed402bad8caa8ee886cc0cf72714a43e2a
Mohamed Azouaoui <azouaoui.med@gmail.com>
2020-11-05 16:14:45 +0100

    update auth with translation

--- src/features/Auth/Auth.tsx
+++ src/features/Auth/Auth.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Input, Form, Typography, Alert } from 'antd';
+import { Button, Input, Form, Alert } from 'antd';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { useTranslation } from 'react-i18next';
@@ -38,7 +38,6 @@ const Auth: React.FC = () => {
   const initialValues: LoginParams = { phone: '', password: '' };
 
   const validationSchema = Yup.object().shape({
-    // email: Yup.string().required(t('required field')).email(t('must be a valid email')),
     phone: Yup.string().required(t('required field')),
     password: Yup.string().required(t('required field')),
   });
@@ -72,7 +71,7 @@ const Auth: React.FC = () => {
                 <img src={logo} alt="logo" />
               </Spacer>
               <Spacer size="xs" direction="vertical">
-                <Label title={t('phone')} error={errors.phone} />
+                <Label title={t('phone')} error={touched.phone ? errors.phone : undefined} />
                 <Form.Item
                   validateStatus={touched.phone && Boolean(errors.phone) ? 'error' : undefined}
                 >
@@ -88,20 +87,29 @@ const Auth: React.FC = () => {
               </Spacer>
 
               <Spacer size="xs" direction="vertical">
-                <Typography.Text>{t('password')}</Typography.Text>
-                <Input.Password
-                  prefix={<Icon name="lock-2" />}
-                  name="password"
-                  value={values.password}
-                  placeholder={t('enter your password')}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  iconRender={(visible) =>
-                    visible ? <Icon name="eye" /> : <Icon name="eye-off" />
-                  }
+                <Label
+                  title={t('password')}
+                  error={touched.password ? errors.password : undefined}
                 />
+                <Form.Item
+                  validateStatus={
+                    touched.password && Boolean(errors.password) ? 'error' : undefined
+                  }
+                >
+                  <Input.Password
+                    prefix={<Icon name="lock-2" />}
+                    name="password"
+                    value={values.password}
+                    placeholder={t('enter your password')}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    iconRender={(visible) =>
+                      visible ? <Icon name="eye" /> : <Icon name="eye-off" />
+                    }
+                  />
+                </Form.Item>
                 <Link style={{ fontSize: 12 }} onClick={() => console.log('forgot password')}>
-                  Forgot password?
+                  {t('forgot password')}
                 </Link>
               </Spacer>
 
@@ -111,7 +119,7 @@ const Auth: React.FC = () => {
                 </Button>
 
                 <Text size="sm" style={{ margin: '0 auto', textAlign: 'center', display: 'block' }}>
-                  {t('dont have an account')} <Link>Sign in</Link>
+                  {t('dont have an account')} <Link>{t('sign in')}</Link>
                 </Text>
               </Spacer>
             </Spacer>
