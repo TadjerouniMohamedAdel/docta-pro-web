401c9a75092e76ff7d69ec7e69db9543901e7f36
Mohamed Azouaoui <azouaoui.med@gmail.com>
2020-11-09 10:56:26 +0100

    add search input

--- src/components/Layout/components/Header/Header.tsx
+++ src/components/Layout/components/Header/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Col, Row } from 'antd';
+import { AutoComplete, Col, Input, Row } from 'antd';
 import UserProfile from './components/UserProfile/UserProfile';
 import SidebarCollapser from './components/SidebarCollapser/SidebarCollapser';
 import SidebarToggler from './components/SidebarToggler/SidebarToggler';
 import Localization from './components/Localization';
+import Icon from '../../../Icon/Icon';
 
 type Props = {
   handleCollapsed: () => void;
@@ -15,9 +16,18 @@ const Header: React.FC<Props> = ({ handleCollapsed, handleToggled, collapsed }) 
   return (
     <header className="header" style={{ display: 'flex', alignItems: 'center', padding: 20 }}>
       <Row style={{ width: '100%', alignItems: 'center' }} justify="space-between">
-        <Col flex="auto">
+        <Col style={{ marginRight: 15 }}>
           <SidebarCollapser handleCollapsed={handleCollapsed} collapsed={collapsed} />
           <SidebarToggler handleToggled={handleToggled} />
+        </Col>
+        <Col flex="auto">
+          <AutoComplete allowClear style={{ maxWidth: 500, width: '100%' }}>
+            <Input
+              size="middle"
+              placeholder="Search patients, staff, invoices etc…"
+              prefix={<Icon name="search" />}
+            />
+          </AutoComplete>
         </Col>
 
         <Col>
