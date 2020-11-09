4aef8b28b6744ee9977b3a325bb7afe71ec01bf2
Mohamed Azouaoui <azouaoui.med@gmail.com>
2020-11-09 11:02:47 +0100

    create search component

--- src/components/Layout/components/Header/Header.tsx
+++ src/components/Layout/components/Header/Header.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { AutoComplete, Col, Input, Row } from 'antd';
+import { Col, Row } from 'antd';
 import UserProfile from './components/UserProfile/UserProfile';
 import SidebarCollapser from './components/SidebarCollapser/SidebarCollapser';
 import SidebarToggler from './components/SidebarToggler/SidebarToggler';
 import Localization from './components/Localization';
-import Icon from '../../../Icon/Icon';
+import Search from './components/Search/Search';
 
 type Props = {
   handleCollapsed: () => void;
@@ -21,13 +21,7 @@ const Header: React.FC<Props> = ({ handleCollapsed, handleToggled, collapsed }) 
           <SidebarToggler handleToggled={handleToggled} />
         </Col>
         <Col flex="auto">
-          <AutoComplete allowClear style={{ maxWidth: 500, width: '100%' }}>
-            <Input
-              size="middle"
-              placeholder="Search patients, staff, invoices etc…"
-              prefix={<Icon name="search" />}
-            />
-          </AutoComplete>
+          <Search />
         </Col>
 
         <Col>
