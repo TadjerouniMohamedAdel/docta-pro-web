0efd54dac1e55cf32d69e477cfc90319bc66fda3
Mohamed Azouaoui <azouaoui.med@gmail.com>
2020-11-10 16:10:01 +0100

    create calendar component

---
+++ src/features/Appointments/components/Calendar/Calendar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Calendar as AntCalendar, Col, Row } from 'antd';
+import moment from 'moment';
+import Text from '../../../../components/Text/Text';
+import './styles.less';
+
+export type Props = {
+  appointmentsCount: number;
+  date: Date;
+  currentDate: Date;
+  onSelectDate: (value: Date) => void;
+};
+
+const Spacer: React.FC<Props> = ({ appointmentsCount, date, onSelectDate, currentDate }) => {
+  return (
+    <div className={`${currentDate === date ? '' : 'inactive-calendar'}`}>
+      <AntCalendar
+        defaultValue={moment(date)}
+        value={moment(date)}
+        fullscreen={false}
+        onSelect={(value) => onSelectDate(value.toDate())}
+        headerRender={() => (
+          <Row justify="space-between" style={{ padding: '28px 16px' }}>
+            <Col>
+              <Text size="lg" style={{ fontWeight: 'bold' }}>
+                {moment(date).format('MMMM YYYY')}
+              </Text>
+            </Col>
+            <Col>{appointmentsCount} Appointments</Col>
+          </Row>
+        )}
+      />
+    </div>
+  );
+};
+
+export default Spacer;
