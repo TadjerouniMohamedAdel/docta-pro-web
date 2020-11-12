573ca4c6ce243e2670e4419477d1068a2cdb8fd9
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2020-11-12 21:03:06 +0100

    create button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button as AntButton } from 'antd';
+import { ButtonProps } from 'antd/lib/button';
+import classNames from 'classnames';
+import './styles.less';
+
+export type Props = Omit<ButtonProps, 'icon'> & {
+  children: React.ReactNode;
+  icon?: React.ReactNode;
+  active?: boolean;
+};
+
+const Button: React.FC<Props> = ({ children, icon, style, size, active = false, ...rest }) => {
+  return (
+    <AntButton
+      className={classNames('custom-button', { 'small-button': size === 'small', active })}
+      icon={icon}
+      style={{ display: 'flex', ...style }}
+      size={size}
+      {...rest}
+    >
+      {children}
+    </AntButton>
+  );
+};
+
+export default Button;
