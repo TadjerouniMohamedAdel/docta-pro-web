1798c747edfed38185ced23bd7737ddb67f30262
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2020-11-12 21:04:07 +0100

    add 'none' type option

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export type Props = {
   name: string;
-  type?: 'fill' | 'line';
+  type?: 'fill' | 'line' | 'none';
   size?: number;
   style?: React.CSSProperties;
 };
@@ -10,7 +10,7 @@ export type Props = {
 const Spacer: React.FC<Props> = ({ name, type = 'line', size = 22, style, ...rest }) => {
   return (
     <svg className="remix" style={{ width: size, height: size, ...style }} {...rest}>
-      <use xlinkHref={`remixicon.symbol.svg#ri-${name}-${type}`} />
+      <use xlinkHref={`remixicon.symbol.svg#ri-${name}${type === 'none' ? '' : `-${type}`}`} />
     </svg>
   );
 };
