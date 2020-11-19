a38e931426f91c8b2d062ee72245cacf8c107047
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2020-11-19 20:01:15 +0100

    add input display on click add new

--- src/features/Patients/components/MedicalRecords/components/RecordBox/RecordBox.tsx
+++ src/features/Patients/components/MedicalRecords/components/RecordBox/RecordBox.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Input } from 'antd';
 import Button from '../../../../../../components/Button/Button';
 import Text from '../../../../../../components/Text/Text';
 import Icon from '../../../../../../components/Icon/Icon';
@@ -11,22 +12,47 @@ type Props = {
 };
 
 const RecordBox: React.FC<Props> = ({ data, title }) => {
+  const [focused, setFocused] = useState<boolean>(false);
+  const [value, setValue] = useState<string>('');
+
+  const handleAddNewValue = () => {
+    alert(value);
+    setFocused(false);
+    setValue('');
+  };
+
   return (
     <div className="Record-box">
       <div className="record-header">
         <Text>{title}</Text>
       </div>
       <div className="record-body">
-        <Button
-          type="ghost"
-          size="small"
-          block
-          icon={<Icon name="add-circle" type="fill" size={18} />}
-          className="btn-record"
-          style={{ marginBottom: 10 }}
-        >
-          Add new
-        </Button>
+        <div style={{ marginBottom: 10, width: '100%' }}>
+          {focused ? (
+            <Input
+              autoFocus
+              size="small"
+              name="value"
+              value={value}
+              placeholder=""
+              onChange={(e) => setValue(e.target.value)}
+              onBlur={() => setFocused(false)}
+              onPressEnter={handleAddNewValue}
+              style={{ width: '100%' }}
+            />
+          ) : (
+            <Button
+              type="ghost"
+              size="small"
+              block
+              icon={<Icon name="add-circle" type="fill" size={18} />}
+              className="btn-record"
+              onClick={() => setFocused(true)}
+            >
+              Add new
+            </Button>
+          )}
+        </div>
         {data.map((item) => (
           <RecordItem>{item}</RecordItem>
         ))}
