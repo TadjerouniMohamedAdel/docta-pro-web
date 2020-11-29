09c622471d681859b9924c865c79f3ffcda964d1
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2020-11-29 16:27:27 +0100

    add refresh token handler

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -34,10 +34,15 @@ function fetcher(endpoint: string, { body, ...customConfig }: any = {}) {
       window.location.assign(window.location as any);
       return Promise.reject(new Error('You need to re-authenticate !'));
     }
-    console.log(response);
     const data = await response.json();
 
+    const responseHeaders: Headers & {
+      authorization?: string;
+    } = response.headers;
+
     if (response.ok) {
+      const authorization: string | null = responseHeaders.get('authorization');
+      if (authorization) localStorage.setItem('token', authorization);
       return data;
     }
     return Promise.reject(data);
