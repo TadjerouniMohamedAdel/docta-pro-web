0ed2083b1081970b1d4c37aabde3252233387d69
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2020-12-16 19:34:37 +0100

    refactore protected component

--- src/features/Auth/ProtectedComponent/ProtectedComponent.tsx
+++ src/features/Auth/ProtectedComponent/ProtectedComponent.tsx
@@ -3,17 +3,34 @@ import { useAuthState } from '../context';
 
 type Props = {
   children: React.ReactNode;
+  type?: 'section' | 'permission';
   accessCode: string;
 };
 
-const ProtectedComponent: React.FC<Props> = ({ children, accessCode, ...rest }) => {
+const ProtectedComponent: React.FC<Props> = ({
+  children,
+  type = 'permission',
+  accessCode,
+  ...rest
+}) => {
   const { user } = useAuthState();
   const [accessible, setAccessible] = useState(false);
 
   const CheckAccess = () => {
     if (user) {
-      const permission = user.permissions.find((item) => item.code === accessCode);
-      if (permission) setAccessible(true);
+      switch (type) {
+        case 'section':
+          if (user.permissions.find((item) => item.section.code === accessCode))
+            setAccessible(true);
+          break;
+
+        case 'permission':
+          if (user.permissions.find((item) => item.code === accessCode)) setAccessible(true);
+          break;
+
+        default:
+          break;
+      }
     }
   };
 
