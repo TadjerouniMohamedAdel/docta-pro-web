7743c566498719aceb47a00964a1b7b4bcd902d6
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2020-12-26 19:52:29 +0100

    add blocked patients listing

--- src/features/Patients/services.ts
+++ src/features/Patients/services.ts
@@ -3,3 +3,7 @@ import fetcher from '../../utils/fetcher';
 export const fetchAllPatients = async (term = '', page = 0, size = 10): Promise<any> => {
   return fetcher(`/api/v1/professionals/patients?term=${term}&skip=${page}&take=${size}`);
 };
+
+export const fetchBlockedPatients = async (term = '', page = 0, size = 10): Promise<any> => {
+  return fetcher(`/api/v1/professionals/patients/blocked?term=${term}&skip=${page}&take=${size}`);
+};
