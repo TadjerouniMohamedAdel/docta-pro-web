5fe66098211fdc099bf50e7afc1d00a0c1c52b9d
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2020-12-28 19:00:38 +0100

    fix typing error

--- src/features/Patients/PatientsList/PatientsList.tsx
+++ src/features/Patients/PatientsList/PatientsList.tsx
@@ -5,16 +5,16 @@ import BlockedPatients from './BlockedPatients/BlockedPatients';
 import AllPatients from './AllPatients/AllPatients';
 
 type Props = {
-  setPatientsCount: (value: number) => void;
+  handleSetPatientCount: (value: number) => void;
 };
 
-const PatientsList: React.FC<Props> = ({ setPatientsCount }) => {
+const PatientsList: React.FC<Props> = ({ handleSetPatientCount }) => {
   const { t } = useTranslation('translation');
 
   return (
     <Tabs defaultActiveKey="1" tabBarStyle={{ paddingLeft: 16, paddingRight: 16 }} size="small">
       <Tabs.TabPane tab={t('all patients')} key="1">
-        <AllPatients setPatientsCount={setPatientsCount} />
+        <AllPatients handleSetPatientCount={handleSetPatientCount} />
       </Tabs.TabPane>
       <Tabs.TabPane tab={t('blocked patients')} key="2">
         <BlockedPatients />
