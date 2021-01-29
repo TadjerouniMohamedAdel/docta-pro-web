05f77987bb34f0eab27a4c2ff72457723fca1360
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2021-01-29 23:35:24 +0100

    intergate add / remove morning horus

--- src/features/Settings/Schedule/WorkingHours/WorkingHoursItem/WorkingHoursItem.tsx
+++ src/features/Settings/Schedule/WorkingHours/WorkingHoursItem/WorkingHoursItem.tsx
@@ -35,6 +35,29 @@ const WorkingHoursItem: React.FC<Props> = ({ workingHourDetails, handleUpdateDat
     handleUpdateData(workingHourDetailsUpdated.dayOfWeek, workingHourDetailsUpdated);
   };
 
+  const handleToggleMorningHours = (action: 'add' | 'remove', type: 'morning' | 'afternoon') => {
+    const workingHourDetailsUpdated: WorkingHoursSchedule = { ...workingHourDetails };
+
+    if (type === 'morning') {
+      if (action === 'add') {
+        workingHourDetailsUpdated.morningStart = '08:00';
+        workingHourDetailsUpdated.morningEnd = '12:00';
+      } else {
+        workingHourDetailsUpdated.morningStart = null;
+        workingHourDetailsUpdated.morningEnd = null;
+      }
+    } else if (action === 'add') {
+      workingHourDetailsUpdated.afternoonStart = '13:00';
+      workingHourDetailsUpdated.afternoonEnd = '17:00';
+    } else {
+      workingHourDetailsUpdated.afternoonStart = null;
+      workingHourDetailsUpdated.afternoonEnd = null;
+    }
+
+    workingHourDetailsUpdated.isEdited = true;
+    handleUpdateData(workingHourDetailsUpdated.dayOfWeek, workingHourDetailsUpdated);
+  };
+
   return (
     <Row style={{ width: '100%' }} gutter={24}>
       <Col span={4}>
@@ -67,26 +90,57 @@ const WorkingHoursItem: React.FC<Props> = ({ workingHourDetails, handleUpdateDat
         {workingHourDetails.isOpen ? (
           <>
             <Row style={{ width: '100%' }} gutter={16} align="middle">
-              <Col span={10}>
-                <div style={{ marginBottom: 4 }}>
-                  <Text style={{ marginBottom: 4 }}>{t('Morning starts at')}</Text>
-                  <HoursSelector
-                    type="morning"
-                    time={workingHourDetails.morningStart}
-                    onChange={(value) => handleChange('morningStart', value as never)}
-                  />
-                </div>
-              </Col>
-              <Col span={10}>
-                <div style={{ marginBottom: 4 }}>
-                  <Text style={{ marginBottom: 4 }}>{t('Morning ends at')}</Text>
-                  <HoursSelector
-                    type="morning"
-                    time={workingHourDetails.morningEnd}
-                    onChange={(value) => handleChange('morningEnd', value as never)}
-                  />
-                </div>
-              </Col>
+              {!workingHourDetails.morningStart || !workingHourDetails.morningEnd ? (
+                <Col span={10}>
+                  <div>
+                    <Button
+                      type="ghost"
+                      size="small"
+                      icon={<Icon name="add-circle-fill" size={18} />}
+                      className="btn-record"
+                      style={{ marginBottom: 4 }}
+                      onClick={() => handleToggleMorningHours('add', 'morning')}
+                    >
+                      {t('Add morning hours')}
+                    </Button>
+                  </div>
+                </Col>
+              ) : (
+                <>
+                  <Col span={10}>
+                    <div style={{ marginBottom: 4 }}>
+                      <Text style={{ marginBottom: 4 }}>{t('Morning starts at')}</Text>
+                      <HoursSelector
+                        type="morning"
+                        time={workingHourDetails.morningStart}
+                        onChange={(value) => handleChange('morningStart', value as never)}
+                      />
+                    </div>
+                  </Col>
+                  <Col span={10}>
+                    <div style={{ marginBottom: 4 }}>
+                      <Text style={{ marginBottom: 4 }}>{t('Morning ends at')}</Text>
+                      <HoursSelector
+                        type="morning"
+                        time={workingHourDetails.morningEnd}
+                        onChange={(value) => handleChange('morningEnd', value as never)}
+                      />
+                    </div>
+                  </Col>
+                  <Col span={4}>
+                    <Button
+                      type="text"
+                      className="btn-record-delete"
+                      danger
+                      size="small"
+                      style={{ marginTop: 18 }}
+                      onClick={() => handleToggleMorningHours('remove', 'morning')}
+                    >
+                      <Icon name="close-circle-line" size={18} />
+                    </Button>
+                  </Col>
+                </>
+              )}
             </Row>
             <Row style={{ width: '100%' }} gutter={16} align="middle">
               {!workingHourDetails.afternoonStart || !workingHourDetails.afternoonEnd ? (
@@ -97,10 +151,7 @@ const WorkingHoursItem: React.FC<Props> = ({ workingHourDetails, handleUpdateDat
                       size="small"
                       icon={<Icon name="add-circle-fill" size={18} />}
                       className="btn-record"
-                      onClick={() => {
-                        handleChange('afternoonStart', '08:00' as never);
-                        handleChange('afternoonEnd', '08:00' as never);
-                      }}
+                      onClick={() => handleToggleMorningHours('add', 'afternoon')}
                     >
                       {t('Add afternoon hours')}
                     </Button>
@@ -135,10 +186,7 @@ const WorkingHoursItem: React.FC<Props> = ({ workingHourDetails, handleUpdateDat
                       danger
                       size="small"
                       style={{ marginTop: 18 }}
-                      onClick={() => {
-                        handleChange('afternoonStart', null as never);
-                        handleChange('afternoonEnd', null as never);
-                      }}
+                      onClick={() => handleToggleMorningHours('remove', 'afternoon')}
                     >
                       <Icon name="close-circle-line" size={18} />
                     </Button>
--- src/features/Settings/Schedule/types.ts
+++ src/features/Settings/Schedule/types.ts
@@ -1,10 +1,10 @@
 export type WorkingHoursSchedule = {
   id: string;
   dayOfWeek: number;
-  morningStart: string;
-  morningEnd: string;
-  afternoonStart: string;
-  afternoonEnd: string;
+  morningStart: string | null;
+  morningEnd: string | null;
+  afternoonStart: string | null;
+  afternoonEnd: string | null;
   isOpen: boolean;
   isEdited?: boolean;
 };
