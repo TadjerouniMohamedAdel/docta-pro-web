6ca5b9de45d70139b29378371d3f033068329a45
Mohamed AZOUAOUI <azouaoui.med@gmail.com>
2021-06-06 16:25:39 +0100

    update exports

--- src/features/AccountSetup/AccountSetup.tsx
+++ src/features/AccountSetup/AccountSetup.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { useAuthState } from '../Auth';
 import { SetupAccountProvider } from './context';
 import './styles.less';
-import { DoctorProfile1, DoctorProfile2 } from './views';
-import Availability from './views/Availability/Availability';
-import CabinetInfo from './views/CabinetInfo/CabinetInfo';
-import ResetPassword from './views/ResetPassword/ResetPassword';
-import VisitReasons from './views/VisitReasons/VisitReasons';
+import {
+  Availability,
+  CabinetInfo,
+  DoctorProfile1,
+  DoctorProfile2,
+  ResetPassword,
+  VisitReasons,
+} from './views';
 
 const AccountSetup: React.FC = () => {
   const { user } = useAuthState();
--- src/features/AccountSetup/views/index.ts
+++ src/features/AccountSetup/views/index.ts
@@ -1,2 +1,6 @@
 export { default as DoctorProfile1 } from './DoctorProfile1/DoctorProfile1';
 export { default as DoctorProfile2 } from './DoctorProfile2/DoctorProfile2';
+export { default as CabinetInfo } from './CabinetInfo/CabinetInfo';
+export { default as Availability } from './Availability/Availability';
+export { default as VisitReasons } from './VisitReasons/VisitReasons';
+export { default as ResetPassword } from './ResetPassword/ResetPassword';
