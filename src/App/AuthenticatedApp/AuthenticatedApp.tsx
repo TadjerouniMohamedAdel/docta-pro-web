d9ca14c510e1361a9ad540492a151050d3d00f9a
TadjerouniMohamedAdel <mohnagato@gmail.com>
2021-10-24 10:17:22 +0100

    checking account suspended

--- src/App/AuthenticatedApp/AuthenticatedApp.tsx
+++ src/App/AuthenticatedApp/AuthenticatedApp.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import { Modal } from 'antd';
+import { AccountSuspendedContext } from '../../common/context/AccountSuspendedContext';
+import AccountSuspended from '../../components/AccountSuspended/AccountSuspended';
 import AccountSetup from '../../features/AccountSetup';
 import { useAuthState } from '../../features/Auth';
 import { MainLayout } from '../../Layout';
 
+
 const AuthenticatedApp: React.FC = () => {
   const { user } = useAuthState();
-  return user?.setupAccountProgress === -1 || user?.role.code !== 'practitioner' ? (
-    <MainLayout />
-  ) : (
-    <AccountSetup />
+  const { suspended } = React.useContext(AccountSuspendedContext);
+
+  return (
+    <>
+      <Modal
+        visible={suspended}
+        footer={null}
+        centered
+        width={600}
+        closable={false}
+      >
+        <AccountSuspended />
+      </Modal>
+      {
+        user?.setupAccountProgress === -1 || user?.role.code !== 'practitioner' ? (
+          <MainLayout />
+        ) : (
+          <AccountSetup />
+        )
+
+      }
+    </>
   );
 };
 
--- src/features/Auth/components/AuthWrapper/AuthWrapper.tsx
+++ src/features/Auth/components/AuthWrapper/AuthWrapper.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import bgImage from '../../../../assets/img/background.jpg';
+import { Typography } from 'antd';
 import { Spacer, Card } from '../../../../components';
 import illustration from '../../../../assets/img/illustration.png';
 import logo from '../../../../assets/img/logo.png';
+import { AccountSuspendedContext } from '../../../../common/context/AccountSuspendedContext';
+import AccountSuspended from '../../../../components/AccountSuspended/AccountSuspended';
+import bgImage from '../../../../assets/img/background.jpg';
 
 export type Props = {
   children: React.ReactNode;
@@ -10,6 +13,9 @@ export type Props = {
 };
 
 const AuthWrapper: React.FC<Props> = ({ children, style, ...rest }) => {
+  const { suspended, setSuspended } = React.useContext(AccountSuspendedContext);
+  const { Text, Link } = Typography;
+
   return (
     <div
       style={{
@@ -31,15 +37,25 @@ const AuthWrapper: React.FC<Props> = ({ children, style, ...rest }) => {
           maxWidth: '100%',
         }}
       >
-        <Card style={{ padding: '40px 35px 24px', maxWidth: 420 }}>
-          <Spacer size="md" direction="vertical">
-            <Spacer size="md" direction="vertical" align="center">
-              <img src={illustration} alt="illustration" />
-              <img src={logo} alt="logo" />
-            </Spacer>
-            {children}
-          </Spacer>
-        </Card>
+        {
+          suspended ? (
+            <Card key="sdfsd" style={{ padding: '40px 35px 24px', maxWidth: 420, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              <AccountSuspended />
+              <Text strong style={{ marginTop: 31 }}>Go back to <Link onClick={() => setSuspended(false)}>Sign In</Link></Text>
+            </Card>
+          ) : (
+            <Card key="sdfssfsd" style={{ padding: '40px 35px 24px', maxWidth: 420 }}>
+              <Spacer size="md" direction="vertical">
+                <Spacer size="md" direction="vertical" align="center">
+                  <img src={illustration} alt="illustration" />
+                  <img src={logo} alt="logo" />
+                </Spacer>
+                {children}
+              </Spacer>
+            </Card>
+
+          )
+        }
       </div>
     </div>
   );
